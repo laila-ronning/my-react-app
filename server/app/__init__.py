4be9f9f42a6c024f5c94bd90a48338aaf6df47a5
laila-ronning <laila>
2015-05-19 13:37:00 +0200

    server with rest/json for listing deploys

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,4 +1,50 @@
-from flask import Flask
+#!flask/bin/python
+from flask import Flask, jsonify, abort, request, make_response, url_for
 
-app = Flask(__name__)
-from app import views
+app = Flask(__name__, static_url_path = "")
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return "Hello, World!"
+
+@app.route('/node-manager/api/v1.0/deploys', methods = ['GET'])
+def get_deploys():
+    return jsonify( { 'deploys': deploys } )
+
+@app.route('/node-manager/api/v1.0/deploys/<int:deploy_id>', methods = ['GET'])
+def get_deploy(deploy_id):
+    return jsonify( { 'task': deploys[0] } )
+
+@app.route('/node-manager/api/v1.0/deploy/create', methods = ['POST'])
+def create_deploy():
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    deploy = {
+        'id': deploys[-1]['id'] + 1,
+        'name': request.json['name'],
+        'group_id': request.json['group_id'],
+        'artifact_id': request.json['artifact_id'],
+        'version': request.json['version'],
+        'status': True,
+        'result': u'logg..... status finished ok'
+    }
+
+    deploys.append(deploy)
+    return jsonify( { 'deploy': deploy } ), 201
+
+deploys = [
+    {
+        'id': 1,
+        'name': u'name',
+        'group_id': u'group_id',
+        'artifact_id': u'artifact_id',
+        'version': u'version',
+        'status': False,
+        'result': u''
+    }
+]
+
+if __name__ == '__main__':
+    app.run(debug = True)
+
